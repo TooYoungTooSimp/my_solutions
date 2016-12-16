900c170c12b708ae5b4e66bb0369585f541f929f
雷宇辰 <crazymonkeylyc@outlook.com>
2016-12-16 13:42:46 +0800

    bzoj 1024 1028

---
+++ OnlineJudges/lydsy/1024.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+double min(double a, double b) { return a < b ? a : b; };
+double max(double a, double b) { return a > b ? a : b; };
+double dfs(double x, double y, int t)
+{
+    if (t == 1) return max(x / y, y / x);
+    double ans = 1e300;
+    for (int i = 1; i <= t / 2; i++)
+        ans = min(ans, min(
+            max(dfs(x / t * i, y, i), dfs(x / t * (t - i), y, t - i)),
+            max(dfs(x, y / t * i, i), dfs(x, y / t * (t - i), t - i))
+        ));
+    return ans;
+}
+int main()
+{
+    int n, m, k;
+    scanf("%d%d%d", &n, &m, &k);
+    printf("%.6lf", dfs(n, m, k));
+    return 0;
+}
