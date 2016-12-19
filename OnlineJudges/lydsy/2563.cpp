2f9c79ce75d36dd191dd48f6a76911f4abaa971e
雷宇辰 <crazymonkeylyc@outlook.com>
2016-12-19 17:24:51 +0800

    bzoj 1816 2429 2563 3098 （抄题解真的好爽啊但是感觉好堕落啊）

---
+++ OnlineJudges/lydsy/2563.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+int a[10010];
+int main()
+{
+    int n, m, ans = 0;
+    scanf("%d%d", &n, &m);
+    for (int i = 1, x; i <= n; i++)
+    {
+        scanf("%d", &x);
+        ans -= x;
+        a[i] = x << 1;
+    }
+    for (int i = 1, x, y, z; i <= m; i++)
+    {
+        scanf("%d%d%d", &x, &y, &z);
+        ans -= z, a[x] += z, a[y] += z;
+    }
+    std::sort(a + 1, a + n + 1);
+    for (int i = 2; i <= n; i += 2)
+        ans += a[i];
+    printf("%d", ans);
+    return 0;
+}
