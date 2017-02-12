b7111be0601a879a04d16d9e06d1f5349182f626
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2017-02-12 19:41:18 +0800

    Refresh

---
+++ OnlineJudges/poj/2115.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+typedef long long int64;
+void exgcd(int64 a, int64 b, int64 &d, int64 &x, int64 &y)
+{
+    !b ? (x = 1, y = 0, d = a) : (exgcd(b, a % b, d, y, x), y -= x * (a / b));
+}
+int main()
+{
+    int64 A, B, C;
+    int k;
+    while (scanf("%lld%lld%lld%d", &A, &B, &C, &k), A | B | C | k)
+    {
+        int64 a = C, b = B - A, x, y, n = 1ll << k, d;
+        exgcd(a, n, d, x, y);
+        if (b % d)
+            puts("FOREVER");
+        else
+        {
+            x = (x * (b / d)) % n;
+            x = (x % (n / d) + n / d) % (n / d);
+            printf("%lld\n", x);
+        }
+    }
+    return 0;
+}
