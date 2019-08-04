80cac29649dec8c4318e07cd1d67c92d0f7b5891
TooYoungTooSimp <6648049+TooYoungTooSimp@users.noreply.github.com>
2019-08-04 20:46:19 +0800

    Sun, 04 Aug 2019 20:46:19 +0800

--- OnlineJudges/lydsy/lydsy.cpp
+++ OnlineJudges/lydsy/lydsy.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int a[205];
+int Log(int x)
+{
+    for (int i = 0;; i++)
+        if ((1 << i) > x + 1)
+            return i;
+    return -1;
+}
 int main()
 {
+    int n;
+    double k, c;
+    scanf("%d%lf%lf", &n, &k, &c);
+    for (int i = 0; i < n; i++) scanf("%d", a + i);
+    sort(a, a + n);
+    reverse(a, a + n);
+    int sum = 0;
+    double ans = 0;
+    for (int i = 0; i < n; i++) sum += a[i];
+    for (int i = 0; i < n; i++) ans += a[i] * (Log(i) * k + c);
+    printf("%.3lf", ans / sum);
     return 0;
 }
